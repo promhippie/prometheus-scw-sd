8bde233f0529e4cefdb362ecdc6d38590ec0cc9d
Thomas Boerger <thomas@webhippie.de>
2026-08-05 12:30:57 +0200

    feat: upgrade prometheus library and change adapter call

--- pkg/action/server.go
+++ pkg/action/server.go
@@ -126,7 +126,16 @@ func Server(cfg *config.Config, logger *slog.Logger) error {
 			lasts:          make(map[string]struct{}),
 		}
 
-		a := adapter.NewAdapter(ctx, cfg.Target.File, "scaleway-sd", disc, logger)
+		a, err := adapter.NewAdapter(ctx, cfg.Target.File, "scaleway-sd", disc, logger, registry)
+
+		if err != nil {
+			logger.Error("Failed to create adapter",
+				"err", err,
+			)
+
+			return err
+		}
+
 		a.Run()
 	}
 
